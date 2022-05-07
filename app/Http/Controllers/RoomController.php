551b3d4b89699cd3f1396e710ccc92c493bda181
Luieka224 <luiejohnmalimit@gmail.com>
2022-05-07 21:00:41 +0800

    Added Room Model and Controller

---
+++ app/Http/Controllers/RoomController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Room;
+use Illuminate\Http\Request;
+
+class RoomController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $rooms = Room::paginate(10);
+
+        return response($rooms, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'floor_id' => 'bail|required|integer',
+            'user_id' => 'integer',
+            'room_type_id' => 'integer',
+            'number' => 'required|integer',
+            'no_of_bed' => 'required|integer',
+            'price' => 'required|gte:0',
+            'is_available' => 'required|boolean',
+        ]);
+
+        Room::create($request->all());
+
+        return response('Success', 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Room  $room
+     * @return \Illuminate\Http\Response
+     */
+    public function show($room)
+    {
+        try {
+            Room::findOrFail($room);
+        } catch (\Exception $ex) {
+            return response(
+                [
+                    'message'=>'Room ID does not exist!',
+                    'exception'=>$ex->getMessage()
+                ], 404);
+        }
+
+        return response(Room::findOrFail($room), 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Room  $room
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Room $room)
+    {
+        $room->update($request->all());
+
+        return response('Successs', 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Room  $room
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Room $room)
+    {
+        try {
+            Room::findOrFail($room);
+        } catch (\Exception $ex) {
+            return response(
+                [
+                    'message'=>'Room ID does not exist!',
+                    'exception'=>$ex->getMessage()
+                ], 404);
+        }
+
+        Room::find($room)->delete();
+
+        return response('Success', 200);
+    }
+}
---
+++ app/Models/Room.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Room extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'floor_id',
+        'user_id',
+        'room_type_id',
+        'number',
+        'no_of_bed',
+        'price',
+        'is_available'
+    ];
+}
