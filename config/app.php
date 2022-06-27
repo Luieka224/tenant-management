c0ce645e21eb430d969aa8f97e219ca9cab6f717
Luieka224 <luiejohnmalimit@gmail.com>
2022-06-28 01:44:18 +0800

    Configure fortify

--- config/app.php
+++ config/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Providers\FortifyServiceProvider;
 use Illuminate\Support\Facades\Facade;
 
 return [
@@ -195,6 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class,
     ],
 
     /*
