865a0e4e5f937245482ccc214ecdd9dbfc79a1ac
Luieka224 <luiejohnmalimit@gmail.com>
2022-06-28 19:29:17 +0800

    Added factories to existing models

---
+++ database/factories/RoomFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Room>
+ */
+class RoomFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'number' => $this->faker->buildingNumber(),
+            'no_of_bed' => $this->faker->numberBetween(1, 4),
+            'price' => $this->faker->randomFloat(2, 10000, 99999),
+            'is_available' => $this->faker->numberBetween(0,1),
+        ];
+    }
+}
